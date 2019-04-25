4c921c62d6f5b43803edd94143bd4c4468b1cbf8
Florencia Miranda <flo.mirandai@gmail.com>
2019-04-25 12:59:55 +0200

    Initial Commit

---
+++ src/Controller/CuestionController.php
@@ -0,0 +1,379 @@
+<?php
+/**
+ * PHP version 7.2
+ * apiTDWUsers - src/Controller/CuestionController.php
+ */
+
+namespace TDW\GCuest\Controller;
+
+use OpenApi\Annotations as OA;
+use Psr\Container\ContainerInterface;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Slim\Http\StatusCode;
+use TDW\GCuest\Entity\Usuario;
+use TDW\GCuest\Entity\Cuestion;
+use TDW\GCuest\Error;
+use TDW\GCuest\Utils;
+
+/**
+ * Class CuestionController
+ */
+class CuestionController
+{
+    /** @var string ruta api gestión cuestiones  */
+    public const PATH_USUARIOS = '/questions';
+
+    /** @var ContainerInterface $container */
+    protected $container;
+
+    /** @var \Firebase\JWT\JWT */
+    protected $jwt;
+
+    /** @var \Monolog\Logger $logger */
+    protected $logger;
+
+    // constructor receives container instance
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+        $this->jwt = $this->container->get('jwt');
+        $this->logger = $this->container->get('logger');
+    }
+
+    /**
+     * Summary: Returns all questions
+     *
+     * @OA\Get(
+     *     path        = "/questions",
+     *     tags        = { "Questions" },
+     *     summary     = "Returns all questions",
+     *     description = "Returns all questions from the system that the user has access to.",
+     *     operationId = "tdw_cget_questions",
+     *     security    = {
+     *          { "TDWApiSecurity": {} }
+     *     },
+     *     @OA\Response(
+     *          response    = 200,
+     *          description = "Array of questions",
+     *          @OA\JsonContent(
+     *              ref  = "#/components/schemas/QuestionsArray"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *          response    = 401,
+     *          ref         = "#/components/responses/401_Standard_Response"
+     *     ),
+     *     @OA\Response(
+     *          response    = 403,
+     *          ref         = "#/components/responses/403_Forbidden_Response"
+     *     ),
+     *     @OA\Response(
+     *          response    = 404,
+     *          ref         = "#/components/responses/404_Resource_Not_Found_Response"
+     *     )
+     * )
+     * @param Request $request
+     * @param Response $response
+     * @return Response
+     */
+    public function cget(Request $request, Response $response): Response
+    {
+        if (0 === $this->jwt->user_id) {
+            return Error::error($this->container, $request, $response, StatusCode::HTTP_FORBIDDEN);
+        }
+
+        // TODO
+        return Error::error($this->container, $request, $response, StatusCode::HTTP_NOT_IMPLEMENTED);
+    }
+
+    /**
+     * Summary: Returns a question based on a single ID
+     *
+     * @OA\Get(
+     *     path        = "/questions/{questionId}",
+     *     tags        = { "Questions" },
+     *     summary     = "Returns a question based on a single ID",
+     *     description = "Returns the question identified by `questionId`.",
+     *     operationId = "tdw_get_questions",
+     *     @OA\Parameter(
+     *          ref    = "#/components/parameters/questionId"
+     *     ),
+     *     security    = {
+     *          { "TDWApiSecurity": {} }
+     *     },
+     *     @OA\Response(
+     *          response    = 200,
+     *          description = "Question",
+     *          @OA\JsonContent(
+     *              ref  = "#/components/schemas/Question"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *          response    = 401,
+     *          ref         = "#/components/responses/401_Standard_Response"
+     *     ),
+     *     @OA\Response(
+     *          response    = 403,
+     *          ref         = "#/components/responses/403_Forbidden_Response"
+     *     ),
+     *     @OA\Response(
+     *          response    = 404,
+     *          ref         = "#/components/responses/404_Resource_Not_Found_Response"
+     *     )
+     * )
+     * @param Request $request
+     * @param Response $response
+     * @param array $args
+     * @return Response
+     */
+    public function get(Request $request, Response $response, array $args): Response
+    {
+        if (0 === $this->jwt->user_id) {
+            return Error::error($this->container, $request, $response, StatusCode::HTTP_FORBIDDEN);
+        }
+
+        // TODO
+        return Error::error($this->container, $request, $response, StatusCode::HTTP_NOT_IMPLEMENTED);
+    }
+
+    /**
+     * Summary: Deletes a question
+     *
+     * @OA\Delete(
+     *     path        = "/questions/{questionId}",
+     *     tags        = { "Questions" },
+     *     summary     = "Deletes a question",
+     *     description = "Deletes the question identified by `questionId`.",
+     *     operationId = "tdw_delete_questions",
+     *     parameters={
+     *          { "$ref" = "#/components/parameters/questionId" }
+     *     },
+     *     security    = {
+     *          { "TDWApiSecurity": {} }
+     *     },
+     *     @OA\Response(
+     *          response    = 204,
+     *          description = "Question deleted &lt;Response body is empty&gt;"
+     *     ),
+     *     @OA\Response(
+     *          response    = 401,
+     *          ref         = "#/components/responses/401_Standard_Response"
+     *     ),
+     *     @OA\Response(
+     *          response    = 403,
+     *          ref         = "#/components/responses/403_Forbidden_Response"
+     *     ),
+     *     @OA\Response(
+     *          response    = 404,
+     *          ref         = "#/components/responses/404_Resource_Not_Found_Response"
+     *     )
+     * )
+     * @param Request $request
+     * @param Response $response
+     * @param array $args
+     * @return Response
+     */
+    public function delete(Request $request, Response $response, array $args): Response
+    {
+        if (!$this->jwt->isMaestro) { // 403
+            return Error::error($this->container, $request, $response, StatusCode::HTTP_FORBIDDEN);
+        }
+
+        // TODO
+        return Error::error($this->container, $request, $response, StatusCode::HTTP_NOT_IMPLEMENTED);
+    }
+
+    /**
+     * Summary: Provides the list of HTTP supported methods
+     *
+     * @OA\Options(
+     *     path        = "/questions",
+     *     tags        = { "Questions" },
+     *     summary     = "Provides the list of HTTP supported methods",
+     *     description = "Return a `Allow` header with a comma separated list of HTTP supported methods.",
+     *     operationId = "tdw_options_questions",
+     *     @OA\Response(
+     *          response    = 200,
+     *          description = "`Allow` header &lt;Response body is empty&gt;",
+     *          @OA\Header(
+     *              header      = "Allow",
+     *              description = "List of HTTP supported methods",
+     *              @OA\Schema(
+     *                  type="string"
+     *              )
+     *          )
+     *     )
+     * )
+     *
+     * @OA\Options(
+     *     path        = "/questions/{questionId}",
+     *     tags        = { "Questions" },
+     *     summary     = "Provides the list of HTTP supported methods",
+     *     description = "Return a `Allow` header with a comma separated list of HTTP supported methods.",
+     *     operationId = "tdw_options_questions_id",
+     *     parameters={
+     *          { "$ref" = "#/components/parameters/questionId" },
+     *     },
+     *     @OA\Response(
+     *          response    = 200,
+     *          description = "`Allow` header &lt;Response body is empty&gt;",
+     *          @OA\Header(
+     *              header      = "Allow",
+     *              description = "List of HTTP supported methods",
+     *              @OA\Schema(
+     *                  type="string"
+     *              )
+     *          )
+     *     )
+     * )
+     * @param Request $request
+     * @param Response $response
+     * @param array $args
+     * @return Response
+     */
+    public function options(Request $request, Response $response, array $args): Response
+    {
+        $this->logger->info(
+            $request->getMethod() . ' ' . $request->getUri()->getPath()
+        );
+
+        $methods = isset($args['id'])
+            ? [ 'GET', 'PUT', 'DELETE' ]
+            : [ 'GET', 'POST' ];
+
+        return $response
+            ->withAddedHeader(
+                'Allow',
+                implode(', ', $methods)
+            );
+    }
+
+    /**
+     * Summary: Creates a new question
+     *
+     * @OA\Post(
+     *     path        = "/questions",
+     *     tags        = { "Questions" },
+     *     summary     = "Creates a new question",
+     *     description = "Creates a new question",
+     *     operationId = "tdw_post_questions",
+     *     @OA\RequestBody(
+     *         description = "`Question` properties to add to the system",
+     *         required    = true,
+     *         @OA\JsonContent(
+     *             ref = "#/components/schemas/QuestionData"
+     *         )
+     *     ),
+     *     security    = {
+     *          { "TDWApiSecurity": {} }
+     *     },
+     *     @OA\Response(
+     *          response    = 201,
+     *          description = "`Created`: question created",
+     *          @OA\JsonContent(
+     *              ref  = "#/components/schemas/Question"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *          response    = 401,
+     *          ref         = "#/components/responses/401_Standard_Response"
+     *     ),
+     *     @OA\Response(
+     *          response    = 403,
+     *          ref         = "#/components/responses/403_Forbidden_Response"
+     *     ),
+     *     @OA\Response(
+     *          response    = 409,
+     *          description = "`Conflict`: the creator does not exist or is not a teacher.",
+     *          @OA\JsonContent(
+     *              ref  = "#/components/schemas/Message",
+     *              example          = {
+     *                   "code"      = 409,
+     *                   "message"   = "`Conflict`: the creator does not exist or is not a teacher."
+     *              }
+     *         )
+     *     )
+     * )
+     * @param Request $request
+     * @param Response $response
+     * @return Response
+     */
+    public function post(Request $request, Response $response): Response
+    {
+        if (!$this->jwt->isMaestro) {
+            return Error::error($this->container, $request, $response, StatusCode::HTTP_FORBIDDEN);
+        }
+
+        // TODO
+        return Error::error($this->container, $request, $response, StatusCode::HTTP_NOT_IMPLEMENTED);
+    }
+
+    /**
+     * Summary: Updates a question
+     *
+     * @OA\Put(
+     *     path        = "/questions/{questionId}",
+     *     tags        = { "Questions" },
+     *     summary     = "Updates a question",
+     *     description = "Updates the question identified by `questionId`.",
+     *     operationId = "tdw_put_questions",
+     *     @OA\Parameter(
+     *          ref    = "#/components/parameters/questionId"
+     *     ),
+     *     @OA\RequestBody(
+     *         description = "`Question` data to update",
+     *         required    = true,
+     *         @OA\JsonContent(
+     *             ref = "#/components/schemas/QuestionData"
+     *         )
+     *     ),
+     *     security    = {
+     *          { "TDWApiSecurity": {} }
+     *     },
+     *     @OA\Response(
+     *          response    = 209,
+     *          description = "`Content Returned`: question previously existed and is now updated",
+     *          @OA\JsonContent(
+     *              ref = "#/components/schemas/Question"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *          response    = 401,
+     *          ref         = "#/components/responses/401_Standard_Response"
+     *     ),
+     *     @OA\Response(
+     *          response    = 403,
+     *          ref         = "#/components/responses/403_Forbidden_Response"
+     *     ),
+     *     @OA\Response(
+     *          response    = 404,
+     *          ref         = "#/components/responses/404_Resource_Not_Found_Response"
+     *     ),
+     *     @OA\Response(
+     *          response    = 409,
+     *          description = "`Conflict`: the creator does not exist or is not a teacher.",
+     *          @OA\JsonContent(
+     *              ref  = "#/components/schemas/Message",
+     *              example          = {
+     *                   "code"      = 409,
+     *                   "message"   = "`Conflict`: the creator does not exist or is not a teacher."
+     *              }
+     *         )
+     *     )
+     * )
+     * @param Request $request
+     * @param Response $response
+     * @param array $args
+     * @return Response
+     */
+    public function put(Request $request, Response $response, array $args): Response
+    {
+        if (!$this->jwt->isMaestro) { // 403
+            return Error::error($this->container, $request, $response, StatusCode::HTTP_FORBIDDEN);
+        }
+
+        // TODO
+        return Error::error($this->container, $request, $response, StatusCode::HTTP_NOT_IMPLEMENTED);
+    }
+}
